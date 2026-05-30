4fbb7ff56c4e561eb49291f41f21ac0222c5a9cc
Lucileudo Paiva <lucileudodepaivalopes@gmail.com>
2026-05-30 14:35:05 -0300

    Inserção de usuario com POST

--- src/main/java/com/lucileudo/workshopmongo/services/UserService.java
+++ src/main/java/com/lucileudo/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucileudo.workshopmongo.domain.User;
+import com.lucileudo.workshopmongo.dto.UserDTO;
 import com.lucileudo.workshopmongo.repository.UserRepository;
 import com.lucileudo.workshopmongo.services.excepton.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+
+	}
+	public User insert(User obj) {
+		return repo.insert(obj);
 	}
 }
