fcf313168f7ac54e422c2139a83c144faaeba4eb
Lucileudo Paiva <lucileudodepaivalopes@gmail.com>
2026-06-01 20:35:27 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/lucileudo/workshopmongo/services/UserService.java
+++ src/main/java/com/lucileudo/workshopmongo/services/UserService.java
@@ -27,11 +27,17 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 
-	}
-	public User insert(User obj) {
-		return repo.insert(obj);
 	}
 }
