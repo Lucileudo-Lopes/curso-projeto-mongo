a42d0114378ec515d151f0391e33afad9d6ab734
Lucileudo Paiva <lucileudodepaivalopes@gmail.com>
2026-06-04 13:07:56 -0300

    Criando entity Post com user aninhado

--- src/main/java/com/lucileudo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucileudo/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.lucileudo.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.lucileudo.workshopmongo.domain.Post;
 import com.lucileudo.workshopmongo.domain.User;
+import com.lucileudo.workshopmongo.repository.PostRepository;
 import com.lucileudo.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,18 +18,30 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
 		userRepository.deleteAll();
-		
-		User maria = new User(null, "Maria Brown", "maria@gmail.com"); 
-		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
-		User bob = new User(null, "Bob Grey", "bob@gmail.com"); 
-	
+		postRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		Post post1 = new Post(null, sdf.parse("03/06/2026"), "Partiu viagem", "Vou viajar para São Paulo, Abaços!",
+				maria);
+		Post post2 = new Post(null, sdf.parse("04/06/2026"), "Bom dia", "Acordei feliz hoje!", maria);
+
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
+		postRepository.saveAll(Arrays.asList(post1, post2));
+
 	}
-	
+
 }
